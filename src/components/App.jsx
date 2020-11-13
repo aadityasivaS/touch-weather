205d0720665cdb8de109d680265618d3afb57b9e
Aadityasiva <67770098+aadityasivaS@users.noreply.github.com>
2020-11-13 19:03:11 +0530

    Lot of  changes

---
+++ src/components/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import AppBar from './AppBar';
+import FloatingActionButton from './FloatingActionButton';
+export default function App() {
+    return <div>
+        <AppBar />
+        <FloatingActionButton />
+    </div>
+}
